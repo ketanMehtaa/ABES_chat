9be61f23b54d3385f95afecffc574c4fea8eca1a
ketan mehta <ketanmehta244241@gmail.com>
2020-10-06 00:20:16 +0530

    after this i will start file upload section

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -51,15 +51,16 @@
     }
     // method to delete a post from DOM
 
-    let deletePost = function(deleteLink){
-        $(deleteLink).click(function(e){
+    let deletePost = function (deleteLink) {
+        $(deleteLink).click(function (e) {
             e.preventDefault();
             $.ajax({
                 type: 'get',
                 url: $(deleteLink).prop('href'),
-                success: function(data){
+                success: function (data) {
                     $(`#post-${data.data.post_id}`).remove();
-                },error: function(error){
+                },
+                error: function (error) {
                     console.log(error.responseText);
                 }
             });
