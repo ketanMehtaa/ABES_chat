f81a87d22b76cde3fbd5410e43b7cd4381370104
ketan mehta <ketanmehta244241@gmail.com>
2020-10-14 10:56:05 +0530

    setting up socket.io

--- models/user.js
+++ models/user.js
@@ -3,7 +3,6 @@ const multer = require('multer');
 const path = require('path');
 const AVATAR_PATH = path.join('/uploads/users/avatars');
 
-
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -20,7 +19,11 @@ const userSchema = new mongoose.Schema({
     },
     avatar: {
         type: String
-    }
+    },
+    friendships: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Friendship'
+    }]
 }, {
     timestamps: true
 });
